6602fa428ed2371411a5b8221b1deb8510f995de
rowrensyum <lesyum7@gmail.com>
2025-08-03 21:47:40 +0900

    Add files via upload

--- 수열계산.js
+++ 수열계산.js
@@ -1,24 +1,14 @@
-
-
-//입력받은값 순열 만들기
-
-export function 전체수열(입력값){
- const 입력 = parseInt(입력값);
- const 수열 = [];
-for(let a = 1; a<입력; a++){
-  var  수 = a;
-  while(수 != 1) {
-    if(수%2==0){
-      수 = 수/2;
-    }else{
-      수 = 3*수 + 1;
+function 전체수열(입력값) {
+  const 입력 = parseInt(입력값);
+  const 수열 = [];
+  let 수 = 입력;
+  while (수 !== 1) {
+    if (수 % 2 === 0) {
+      수 = 수 / 2;
+    } else {
+      수 = 3 * 수 + 1;
     }
     수열.push(수);
-
   }
-
-return 수열;
+  return 수열;
 }
-}
-
-
